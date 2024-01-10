296e60ba377e85ec0dfe68514508f0840106c5d3
PixoDev <robertogarciadevelopment@gmail.com>
2024-01-10 10:45:19 +0100

    feat: Add SwiftKyber library setup

---
+++ InternxtDesktop/Utils/KyberEncryption.swift
@@ -0,0 +1,20 @@
+//
+//  KyberEncryption.swift
+//  InternxtDesktop
+//
+//  Created by Roberto García on 15/01/24.
+//
+
+// Initialize SwiftKyber library
+import Foundation
+import SwiftKyber
+
+// Entry point for initializing the library
+class KyberInitializer {
+    static func setup() {
+        print("SwiftKyber library initialized")
+    }
+}
+
+KyberInitializer.setup()
+
