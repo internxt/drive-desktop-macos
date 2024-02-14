3d22b439cf0195b9733fee292be0bdb9f56a2003
PixoDev <robertogarciadevelopment@gmail.com>
2024-02-14 11:20:15 +0100

    feat: Implement key generation using SwiftKyber

--- InternxtDesktop/Utils/KyberEncryption.swift
+++ InternxtDesktop/Utils/KyberEncryption.swift
@@ -9,12 +9,24 @@
 import Foundation
 import SwiftKyber
 
-// Entry point for initializing the library
-class KyberInitializer {
-    static func setup() {
-        print("SwiftKyber library initialized")
+class KyberKeyManager {
+    private var keyPair: (publicKey: Data, privateKey: Data)?
+
+    func generateKeys() {
+        let kyber = SwiftKyber()
+        keyPair = kyber.generateKeyPair()
+        print("Keys generated successfully")
+    }
+
+    func getPublicKey() -> Data? {
+        return keyPair?.publicKey
+    }
+
+    func getPrivateKey() -> Data? {
+        return keyPair?.privateKey
     }
 }
 
-KyberInitializer.setup()
+let keyManager = KyberKeyManager()
+keyManager.generateKeys()
 
