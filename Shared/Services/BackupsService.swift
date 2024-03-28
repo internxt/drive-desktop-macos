80595d692a818cdf5dcd284cae7d62d699fbae82
Richard <rascanio@shokworks.io>
2024-03-28 11:06:14 -0400

    default config for realm

--- Shared/Services/BackupsService.swift
+++ Shared/Services/BackupsService.swift
@@ -37,9 +37,10 @@ class BackupsService: ObservableObject {
 
     private func getRealm() -> Realm {
         do {
-            let config = Realm.Configuration(schemaVersion: 2)
-            Realm.Configuration.defaultConfiguration = config
-            return try Realm(fileURL: ConfigLoader.realmURL)
+            return try Realm(configuration: Realm.Configuration(
+                fileURL: ConfigLoader.realmURL,
+                deleteRealmIfMigrationNeeded: true
+            ))
         } catch {
             error.reportToSentry()
             fatalError("Unable to open Realm")
