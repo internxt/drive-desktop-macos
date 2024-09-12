ff04fe325352c89b9bb26468ffd1936eb6708ab5
PixoDev <robertogarciadevelopment@gmail.com>
2024-09-12 09:50:48 +0200

    feat: Add SwiftKyber support for new algorithms

---
+++ InternxtDesktop/Utils/Kyber/KyberAlgorithmManager.swift
@@ -0,0 +1,8 @@
+// Extended support for new algorithms
+// Example implementation of a hypothetical new algorithm
+class KyberAlgorithmManager {
+    func useNewAlgorithm(data: Data) -> Data {
+        // Implementation for new algorithm
+        return data
+    }
+}
