d5dc87ff50e8c565ee4fdc6d7ef82b53b3167f3a
PixoDev <robertogarciadevelopment@gmail.com>
2024-10-23 13:15:25 +0200

    test: Add benchmarking for key generation

---
+++ KyberTests/SwiftKyberBenchmarckTest.swift
@@ -0,0 +1,11 @@
+// Added benchmarking tests for key generation performance.
+import XCTest
+
+class SwiftKyberBenchmarkTests: XCTestCase {
+    func testKeyGenerationPerformance() {
+        measure {
+            let keyManager = KyberKeyManager()
+            keyManager.generateKeys()
+        }
+    }
+}
