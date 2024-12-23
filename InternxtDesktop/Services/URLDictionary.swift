c690f67c44817d4aa0f863d3947e6ec3e875df82
patricioxavier8 <116120453+patricioxavier8@users.noreply.github.com>
2024-12-22 20:13:02 -0500

    add staging url

--- InternxtDesktop/Services/URLDictionary.swift
+++ InternxtDesktop/Services/URLDictionary.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct URLDictionary {
     public static var DRIVE_WEB = URL(string: "https://drive.internxt.com")!
-    public static var WEB_AUTH_SIGNIN = URL(string:"https://drive.internxt.com/login?universalLink=true")!
+    public static var WEB_AUTH_SIGNIN = URL(string:"https://staging.drive.internxt.com/login?universalLink=true")!
     public static var WEB_AUTH_SIGNUP = URL(string:"https://drive.internxt.com/new?universalLink=true")!
     public static var LEARN_MORE_ABOUT_INTERNXT_DRIVE = URL(string:"https://internxt.com/drive")!
     public static var UPGRADE_PLAN = URL(string:"https://drive.internxt.com/preferences?tab=plans")!
